9e7ab41c0ef9dfc3a9d4bf1da4aa83b00ffce1e9
Tom Dinchak <dinchak@gmail.com>
2012-08-03 00:16:31 -0700

    fixes for virtual monome

--- examples/virtual.js
+++ examples/virtual.js
@@ -1,26 +1,48 @@
+var osc = require('osc-min');
+var udp = require('dgram');
 var serialosc = require('./../lib/serialosc.js');
+var stdin = process.openStdin();
+
+var sendMsg = function(addr, args) {
+  var msg = osc.toBuffer({ address: addr, args: args });
+  udp.send(msg, 0, msg.length, device.serialoscPort, '127.0.0.1');
+}
+
+var virtualDevice = serialosc.createVirtualDevice({
+  name: 'test virtual grid',
+  prefix: '/monome',
+  sizeX: 16,
+  sizeY: 8,
+  serialoscPort: 12345,
+});
+virtualDevice.on('stateChange', function() {
+  for (var y = 0; y < virtualDevice.sizeY; y++) {
+    var row = '';
+    for (var x = 0; x < virtualDevice.sizeX; x++) {
+      row += virtualDevice.ledState[x][y];
+    }
+    console.log(row);
+  }
+});
+
 serialosc.on('discover', function(device) {
   console.log("discovered " + device.service.name);
   device.focus();
   device.on('msg', function(addr, args) {
     console.log(device.service.name + ": " + addr + " " + args);
-  })
-})
-serialosc.discover();
-
-var device = serialosc.createVirtualDevice({
-  name: 'test virtual grid',
-  sizeX: 16,
-  sizeY: 8
+    if (addr == "/monome/grid/key/press") {
+      device.msg("/monome/grid/led", [args[0], args[1], args[2]]);
+    }
+  });
 });
 
-var stdin = process.openStdin();
+serialosc.discover();
+
 stdin.on('data', function(chunk) { 
   var cmd = chunk.toString();
-  console.log("enter press data (ie. 3,5,1): "); 
   var matches = cmd.match(/(\d+),(\d+),(\d+)/);
   if (matches) {
-    console.log("sent press");
-    device.press(matches[1], matches[2], matches[3]);
+    virtualDevice.press(parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3]));
   }
+  console.log("enter press data (ie. 3,5,1): "); 
 });
