74cb8ec1a042725d7019b916850fd694654e5fbb
Tom Dinchak <dinchak@gmail.com>
2012-08-03 00:24:35 -0700

    fixed some osc messages, added blinky lights to examples

--- examples/discover.js
+++ examples/discover.js
@@ -4,7 +4,10 @@ serialosc.on('discover', function(device) {
   device.focus();
   device.on('msg', function(addr, args) {
     console.log(device.service.name + ": " + addr + " " + args);
-  })
-})
+    if (addr == "/monome/grid/key") {
+      device.msg("/monome/grid/led/set", [args[0], args[1], args[2]]);
+    }
+  });
+});
 serialosc.discover();
 process.openStdin();
--- examples/virtual.js
+++ examples/virtual.js
@@ -30,8 +30,8 @@ serialosc.on('discover', function(device) {
   device.focus();
   device.on('msg', function(addr, args) {
     console.log(device.service.name + ": " + addr + " " + args);
-    if (addr == "/monome/grid/key/press") {
-      device.msg("/monome/grid/led", [args[0], args[1], args[2]]);
+    if (addr == "/monome/grid/key") {
+      device.msg("/monome/grid/led/set", [args[0], args[1], args[2]]);
     }
   });
 });
