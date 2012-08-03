73910dbc98b4ef09b789942369860cbbff35da6a
Tom Dinchak <dinchak@gmail.com>
2012-08-02 20:14:48 -0700

    added grid/arc detection and tests

--- index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/serialosc.js');
--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,49 @@
-var fluid = require("infusion");
 var jqUnit = require("jqUnit");
 var serialosc = require("../lib/serialosc.js");
 
 jqUnit.module("SerialOSC Tests");
 
-var colors = fluid.registerNamespace("colors");
+jqUnit.test("Detect Devices", function() {
 
-jqUnit.test("Grid Device", function() {
+  // grid tests
   var service = {};
   service.name = "test monome";
   service.port = "1234";
   service.addresses = ['127.0.0.1'];
   serialosc.discoverDevice(service);
-  jqUnit.assert(serialosc.devices[service.name]);
+
+  var sizeMsg = { 
+    address: '/sys/size', 
+    arguments: [
+      { 
+        type: 'integer',
+        value: 8
+      },
+      { 
+        type: 'integer',
+        value: 8
+      }
+    ]
+  };
+
+  serialosc.devices[service.name].recvMsg(sizeMsg);
+  var device = serialosc.devices[service.name];
+  jqUnit.assert(device);
+  jqUnit.assertValue(device.type, 'grid');
+  jqUnit.assertValue(device.encoders, 0);
+  jqUnit.assertValue(device.sizeX, 8);
+  jqUnit.assertValue(device.sizeY, 8);
+
+  // arc tests
+  service = {};
+  service.name = "test arc 2";
+  service.port = "1234";
+  service.addresses = ['127.0.0.1'];
+  serialosc.discoverDevice(service);
+  var device = serialosc.devices[service.name];
+  jqUnit.assert(device);
+  jqUnit.assertValue(device.type, 'arc');
+  jqUnit.assertValue(device.encoders, 2);
+  jqUnit.assertValue(device.sizeX, 0);
+  jqUnit.assertValue(device.sizeY, 0);
 });
