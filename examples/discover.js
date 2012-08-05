82dcd3306fff20f619fc3e528320e92b4602aeef
Tom Dinchak <dinchak@gmail.com>
2012-08-05 14:47:10 -0700

    completely separated virtualDevice from node-serialosc devices

--- examples/discover.js
+++ examples/discover.js
@@ -1,14 +1,19 @@
 var serialosc = require('./../lib/serialosc.js');
+
 serialosc.on('deviceFound', function(device) {
   console.log("found device " + device.service.name);
-  device.focus();
-  device.on('press', function(x, y, s) {
-    console.log('press from ' + device.service.name + ': ' + x + ", " + y + ", " + s);
-    device.oscOut("/monome/grid/led/set", x, y, s);
+  device.focus(function() {
+    device.on('press', function(x, y, s) {
+      console.log('press from ' + device.service.name + ': ' + x + ", " + y + ", " + s);
+      device.oscOut("/monome/grid/led/set", x, y, s);
+    });
   });
 });
+
 serialosc.on('deviceLost', function(device) {
   console.log("lost device " + device.service.name);
 });
+
 serialosc.discover();
+
 process.openStdin();
--- examples/virtual.js
+++ examples/virtual.js
@@ -1,31 +1,37 @@
 var serialosc = require('./../lib/serialosc.js');
 
 var virtualDevice = serialosc.createVirtualDevice({
-  name: 'test virtual grid',
-  prefix: '/monome',
+  name: 'monome 128 (v0000001)',
   sizeX: 16,
-  sizeY: 8,
-  serialoscPort: 1234
+  sizeY: 8
 });
 
 serialosc.on('deviceFound', function(device) {
   console.log("found device " + device.service.name);
-  device.focus();
-  device.on('press', function(x, y, s) {
-    console.log('press from ' + device.service.name + ': ' + x + ", " + y + ", " + s);
-    device.oscOut("/monome/grid/led/set", x, y, s);
-  });
-  device.on('stateChange', function() {
-    console.log('---');
-    for (var y = 0; y < device.sizeY; y++) {
-      var row = '';
-      for (var x = 0; x < device.sizeX; x++) {
-        row += device.gridState[x][y];
-      }
-      console.log(row);
+  device.focus(function() {
+
+    device.on('press', function(x, y, s) {
+      console.log('press from ' + device.service.name + ': ' + x + ", " + y + ", " + s);
+      device.oscOut("/monome/grid/led/set", x, y, s);
+    });
+
+    // print out grid representation if this is our virtual device
+    if (device.service.name == virtualDevice.name) {
+      virtualDevice.on('stateChange', function() {
+        console.log('---');
+        for (var y = 0; y < device.sizeY; y++) {
+          var row = '';
+          for (var x = 0; x < device.sizeX; x++) {
+            row += virtualDevice.gridState[x][y];
+          }
+          console.log(row);
+        }
+      });
     }
+
   });
 });
+
 serialosc.on('deviceLost', function(device) {
   console.log("lost device " + device.service.name);
 });
@@ -37,8 +43,7 @@ stdin.on('data', function(chunk) {
   var cmd = chunk.toString();
   var matches = cmd.match(/(\d+),(\d+),(\d+)/);
   if (matches) {
-    var device = serialosc.devices['test virtual grid'];
-    device.press(parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3]));
+    virtualDevice.press(parseInt(matches[1]), parseInt(matches[2]), parseInt(matches[3]));
   }
   console.log("enter press data (ie. 3,5,1): "); 
 });
