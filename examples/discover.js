a66059d0c671c4a2bcf3c83b93002f2d6c45761a
Tom Dinchak <dinchak@gmail.com>
2012-08-11 12:46:59 -0700

    separated out virtual monome to new node-fakegrid module

--- examples/discover.js
+++ examples/discover.js
@@ -5,7 +5,7 @@ serialosc.on('deviceFound', function(device) {
   device.focus(function() {
     device.on('press', function(x, y, s) {
       console.log('press from ' + device.service.name + ': ' + x + ", " + y + ", " + s);
-      device.oscOut("/monome/grid/led/set", x, y, s);
+      device.oscOut(device.prefix + "/grid/led/set", x, y, s);
     });
   });
 });
